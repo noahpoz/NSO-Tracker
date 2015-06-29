2db8f4a408355ff79d0cd12874ee0849ff869e9e
Christian Koehler <sandvice@gmail.com>
2015-06-28 22:13:41 -0400

    Added two files

--- src/driver/hi.java
+++ src/driver/hi.java
@@ -5,6 +5,8 @@ public class hi {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi");
+		System.out.println("it works!");
+		System.out.println("hiii");
 
 	}
 
